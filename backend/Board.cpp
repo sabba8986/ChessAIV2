18a06465eedcaf9d8281bb41cc182f12a4317120
sabba8986 <sabba.8986@gmail.com>
2026-05-10 14:37:14 -0400

    refactor(backend): change board initialization bitboards to constexpr arrays, initialize board using arrays
    CHANGE: bitboards in Board class now stored in std::array instead of raw C array, for enabling copy initialization

--- backend/Board.cpp
+++ backend/Board.cpp
@@ -5,19 +5,8 @@
 #include <iostream>
 
 Board::Board(){
-    white[PAWN_INDEX] = INIT_WHITE_PAWNS;
-    white[ROOK_INDEX] = INIT_WHITE_ROOKS;
-    white[KNIGHT_INDEX] = INIT_WHITE_KNIGHTS;
-    white[BISHOP_INDEX] = INIT_WHITE_BISHOPS;
-    white[QUEEN_INDEX] = INIT_WHITE_QUEEN;
-    white[KING_INDEX] = INIT_WHITE_KING;
-
-    black[PAWN_INDEX] = INIT_BLACK_PAWNS;
-    black[ROOK_INDEX] = INIT_BLACK_ROOKS;
-    black[KNIGHT_INDEX] = INIT_BLACK_KNIGHTS;
-    black[BISHOP_INDEX] = INIT_BLACK_BISHOPS;
-    black[QUEEN_INDEX] = INIT_BLACK_QUEEN;
-    black[KING_INDEX] = INIT_BLACK_KING;
+    white = defaults::white_init;
+    black = defaults::black_init;
 }
 
 Piece *Board::layout(){
--- backend/Board.hpp
+++ backend/Board.hpp
@@ -1,16 +1,15 @@
 #ifndef Board
 #define BOARD
 
-#include "constants.hpp"
 #include "Piece.hpp"
 #include <cstdint>
-#include <string>
+#include <array>
 
 class Board{
     //bitboards for each color. Pieces are in the order of: 
     //{Pawn, Rook, Knight, Bishop, Queen, King}
-    std::uint64_t white[6];
-    std::uint64_t black[6];
+    std::array<std::uint64_t, 6> white;
+    std::array<std::uint64_t, 6> black;
 
     std::array<Piece, 64> pieces;
 public:

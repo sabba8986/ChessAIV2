f25a799ca35e353f65cbe2ab3636b0de5bbf139c
sabba8986 <sabba.8986@gmail.com>
2026-05-11 19:38:44 -0400

    fix(gui): fix renderBoardPieces to support get_board_state change, move constants to separate .py

---
+++ gui/constants.py
@@ -0,0 +1,22 @@
+PIECES = [[
+    "../assets/icons/pieces/black_pawn.svg",
+    "../assets/icons/pieces/black_rook.svg",
+    "../assets/icons/pieces/black_knight.svg",
+    "../assets/icons/pieces/black_bishop.svg",
+    "../assets/icons/pieces/black_queen.svg",
+    "../assets/icons/pieces/black_king.svg"
+    ], [
+    "../assets/icons/pieces/white_pawn.svg",
+    "../assets/icons/pieces/white_rook.svg",
+    "../assets/icons/pieces/white_knight.svg",
+    "../assets/icons/pieces/white_bishop.svg",
+    "../assets/icons/pieces/white_queen.svg",
+    "../assets/icons/pieces/white_king.svg"
+]]
+
+BROWN = "#B88B4A"
+WHITE = "#E3C16F"
+
+
+TILE_STYLE = "background-color:{color}; border: black; border-width: thin"
+
--- gui/window.py
+++ gui/window.py
@@ -1,13 +1,6 @@
 from PySide6 import QtCore, QtWidgets, QtGui
 import board 
-
-PIECE_TO_ICON = None
-
-BROWN = "#B88B4A"
-WHITE = "#E3C16F"
-
-TILE_STYLE = "background-color:{color}; border: black; border-width: thin"
-
+from constants import PIECES, TILE_STYLE, BROWN, WHITE
 
 class Tile(QtWidgets.QPushButton): 
     def __init__(self, window: QtWidgets.QWidget, pos: tuple[int, int]):
@@ -25,23 +18,6 @@ class Tile(QtWidgets.QPushButton):
 class Window(QtWidgets.QWidget): 
     def __init__(self):
         super().__init__()
-        self.PIECE_TO_ICON = {
-            'k': QtGui.QIcon("../assets/icons/pieces/black_king.svg"), 
-            'q': QtGui.QIcon("../assets/icons/pieces/black_queen.svg"),
-            'r': QtGui.QIcon("../assets/icons/pieces/black_rook.svg"), 
-            'b': QtGui.QIcon("../assets/icons/pieces/black_bishop.svg"),
-            'p': QtGui.QIcon("../assets/icons/pieces/black_pawn.svg"), 
-            'n': QtGui.QIcon("../assets/icons/pieces/black_knight.svg"), 
-
-            'K': QtGui.QIcon("../assets/icons/pieces/white_king.svg"), 
-            'Q': QtGui.QIcon("../assets/icons/pieces/white_queen.svg"), 
-            'R': QtGui.QIcon("../assets/icons/pieces/white_rook.svg"), 
-            'B': QtGui.QIcon("../assets/icons/pieces/white_bishop.svg"), 
-            'P': QtGui.QIcon("../assets/icons/pieces/white_pawn.svg"), 
-            'N': QtGui.QIcon("../assets/icons/pieces/white_knight.svg"), 
-            '_': QtGui.QIcon()
-        }
-
 
 
         self.tileLayout = QtWidgets.QGridLayout()
@@ -69,12 +45,14 @@ class Window(QtWidgets.QWidget):
 
     def renderBoardPieces(self, boardState: str): 
         index = 0
-        for i in range(0, 8):
-            for j in range(0, 8):
+        for i in range(7, -1, -1):
+            for j in range(7, -1, -1):
                 piece = boardState[index]
                 tile = self.tileLayout.itemAtPosition(i, j).widget()
-                tile.setIcon(self.PIECE_TO_ICON[piece]) 
-                
+                if(piece.type == -1):
+                    tile.setIcon(QtGui.QIcon())
+                else:
+                    tile.setIcon(PIECES[1 if piece.is_white else 0][piece.type])  
                 index += 1
      
 

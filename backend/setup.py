e62245eb4d2f23b7ca79ebb526549e37faee900d
sabba8986 <sabba.8986@gmail.com>
2026-05-11 17:29:02 -0400

    fix(backend): replace setup.py with pyproject.toml, fix filenames

--- backend/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, Extension
-
-api_module = Extension('board', sources=['Boardmodule.c', 'interface.cpp', 'Board.cpp'], extra_compile_args=['-std=c++20'], language = 'c++')
-
-setup(name='board', version='1.0', description = "An interface to the ChessV2 game engine.", 
-      ext_modules=[api_module])
-
